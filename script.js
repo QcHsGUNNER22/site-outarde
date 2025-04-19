93a27470ea52d6a59ea689994d496777b07220a9
QcHsGUNNER22 <oliviertailly@hotmail.com>
2025-04-18 22:02:53 -0400

    sounds

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const referenceSounds = {
-    dindon: "sounds/dindon.mp3",
-    bernache: "sounds/bernache.mp3",
-    oie: "sounds/oie.mp3"
+    dindon: "sounds/dindon.wav",
+ 
+    Assembly_Call: "sounds/Assembly_Call.mp3",
+    Cluck: "sounds/Cluck.mp3",
+    Cutting_Hen: "sounds/Cutting_Hen.mp3",
+    Fly_Down_Cackle: "sounds/Fly_Down_Cackle.mp3",
+    Gobbling: "sounds/Gobbling.mp3",
+    Kee_Kee_Run: "sounds/Kee_Kee_Run.mp3",
+    Purr: "sounds/Purr.mp3",
+    Putt: "sounds/Putt.mp3",
+    Tree_Call: "sounds/Tree_Call.mp3",
+    Yelp_Hen: "sounds/Yelp_Hen.mp3",
+    Cluck_Purr: "sounds/Cluck_Purr.mp3",
   };
   
   const animalSelect = document.getElementById("animal");
